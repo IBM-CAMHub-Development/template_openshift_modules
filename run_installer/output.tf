5a9e1543f1a768b6789486c1e16701fd4b38265b
CAMHub PublishBot <octravis@us.ibm.com>
2019-09-18 18:39:09 +0000

    Automated CAMHub::publish on 2019-09-18 18:39:09 UTC

--- run_installer/output.tf
+++ run_installer/output.tf
@@ -1,1 +1,4 @@
-output "dependsOn" { value = "${null_resource.finish_installing.id}" description="Output Parameter when Module Complete"}
+output "dependsOn" { 
+    value = "${null_resource.finish_installing.id}" 
+    description="Output Parameter when Module Complete"
+}
