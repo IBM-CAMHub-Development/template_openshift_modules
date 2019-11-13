209490f99ad27555aebfe800bd275a71b68fea70
CAMHub PublishBot <octravis@us.ibm.com>
2019-11-13 18:53:53 +0000

    Automated CAMHub::publish on 2019-11-13 18:53:53 UTC

---
+++ vmware_ign_config/generate_ign_config.tf
@@ -0,0 +1,113 @@
+resource "null_resource" "generate_ign_config_dependsOn" {
+  provisioner "local-exec" {
+    # Hack to force dependencies to work correctly. Must use the dependsOn var somewhere in the code for dependencies to work. Contain value which comes from previous module.
+	  command = "echo The dependsOn output for apache web server module is ${var.dependsOn}"
+  }
+}
+
+resource "null_resource" "generate_ign_config" {
+  depends_on = ["null_resource.generate_ign_config_dependsOn"]
+  connection {
+    type = "ssh"
+    user = "${var.vm_os_user}"
+    password =  "${var.vm_os_password}"
+    private_key = "${base64decode(var.vm_os_private_key_base64)}"
+    host = "${var.vm_ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${ length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_host_key    = "${var.bastion_host_key}"
+    bastion_password    = "${var.bastion_password}"        
+  }
+
+  provisioner "file" {
+    source = "${path.module}/scripts/config_infra.sh"
+    destination = "/tmp/config_infra.sh"
+  }
+  
+  provisioner "file" {
+    source = "${path.module}/scripts/install-config.yaml.tmpl"
+    destination = "/tmp/install-config.yaml.tmpl"
+  }   
+
+  provisioner "file" {
+    source = "${path.module}/scripts/sec_bootstrap.ign"
+    destination = "/tmp/sec_bootstrap.ign"
+  }   
+  
+  provisioner "remote-exec" {
+    inline = [
+      "set -e",
+      "chmod +x /tmp/config_infra.sh",
+      "bash -c '/tmp/config_infra.sh -h ${var.vm_ipv4_address} -oc ${var.ocversion} -d ${var.domain} -n ${var.controlnodes} -cn ${var.clustername} -vc ${var.vcenter} -vu ${var.vcenteruser} -vp ${var.vcenterpassword} -vd ${var.vcenterdatacenter} -vs ${var.vmwaredatastore} -s ${var.pullsecret}'"
+    ]
+  }
+}
+
+resource "camc_scriptpackage" "get_bootstrap_ign" {
+	depends_on = ["null_resource.generate_ign_config"]
+  	program = ["sudo cat /installer/bootstrap.ign | base64 -w0"]
+  	on_create = true
+    remote_user = "${var.vm_os_user}"
+    remote_password =  "${var.vm_os_password}"
+    remote_key = "${var.vm_os_private_key_base64}"
+    remote_host = "${var.vm_ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_password    = "${var.bastion_password}"            	
+}
+
+resource "camc_scriptpackage" "get_master_ign" {
+	depends_on = ["null_resource.generate_ign_config"]
+  	program = ["sudo cat /installer/master.ign | base64 -w0"]
+  	on_create = true
+    remote_user = "${var.vm_os_user}"
+    remote_password =  "${var.vm_os_password}"
+    remote_key = "${var.vm_os_private_key_base64}"
+    remote_host = "${var.vm_ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_password    = "${var.bastion_password}"            	
+}
+
+resource "camc_scriptpackage" "get_worker_ign" {
+	depends_on = ["null_resource.generate_ign_config"]
+  	program = ["sudo cat /installer/worker.ign | base64 -w0"]
+  	on_create = true
+    remote_user = "${var.vm_os_user}"
+    remote_password =  "${var.vm_os_password}"
+    remote_key = "${var.vm_os_private_key_base64}"
+    remote_host = "${var.vm_ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_password    = "${var.bastion_password}"            	
+}
+
+resource "camc_scriptpackage" "get_bootstrap_sec_ign" {
+	depends_on = ["null_resource.generate_ign_config"]
+  	program = ["sudo cat /installer/sec_bootstrap.ign | base64 -w0"]
+  	on_create = true
+    remote_user = "${var.vm_os_user}"
+    remote_password =  "${var.vm_os_password}"
+    remote_key = "${var.vm_os_private_key_base64}"
+    remote_host = "${var.vm_ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_password    = "${var.bastion_password}"            	
+}
+
+resource "null_resource" "ign_config_generated" {
+  depends_on = ["null_resource.generate_ign_config","null_resource.generate_ign_config_dependsOn"]
+  provisioner "local-exec" {
+    command = "echo 'Ign config created'" 
+  }
+}
---
+++ vmware_ign_config/scripts/config_infra.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+WARN='\033[0;31m'
+REGULAR='\033[0m'
+
+function gen_key()
+{
+	yes y | ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa_ocp
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa_ocp
+}
+
+function get_installer(){
+	sudo mkdir -p /installer
+	sudo curl -o /installer/openshift-install-linux-${1}.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${1}/openshift-install-linux-${1}.tar.gz
+    sudo curl -o /installer/openshift-client-linux-${1}.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${1}/openshift-client-linux-${1}.tar.gz
+    sudo tar xzvf /installer/openshift-install-linux-${1}.tar.gz -C /installer
+    sudo tar xzvf /installer/openshift-client-linux-${1}.tar.gz -C /installer
+    sudo mv /installer/oc /installer/kubectl /installer/openshift-install /usr/local/bin/
+    sudo chmod +x /usr/local/bin/oc /usr/local/bin/kubectl /usr/local/bin/openshift-install
+}
+
+function create_install_config(){
+	SSH_KEY=`sudo cat ~/.ssh/id_rsa_ocp.pub`
+	sudo mv /tmp/install-config.yaml.tmpl /installer/install-config.yaml
+	sudo mv /tmp/sec_bootstrap.ign /installer/sec_bootstrap.ign
+	sudo sed -i -e "s/@domain@/${DOMAIN}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@controlnodes@/${CONTROL_NODES}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@clustername@/${CLUSTER_NAME}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@vcenter@/${VCENTER}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@vcenteruser@/${VCENTER_USER}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@vcenterpassword@/${VCENTER_PASS}/" /installer/install-config.yaml
+	sudo sed -i -e "s/@vcenterdatacenter@/${VCENTER_DC}/" /installer/install-config.yaml
+	sudo sed -i -e "s|@vmwaredatastore@|${VM_DSTORE}|" /installer/install-config.yaml
+	sudo sed -i -e "s/@pullsecret@/${PULL_SECRET_DECODE}/" /installer/install-config.yaml
+	sudo sed -i -e "s|@sshkey@|${SSH_KEY}|" /installer/install-config.yaml
+	sudo cp /installer/install-config.yaml /installer/install-config.yaml.bak
+	sudo /usr/local/bin/openshift-install create manifests --dir=/installer/	
+    sudo sed -i -e "s/mastersSchedulable: true/mastersSchedulable: false/" /installer/manifests/cluster-scheduler-02-config.yml
+    sudo /usr/local/bin/openshift-install create ignition-configs --dir=/installer/
+    if [ -d "/var/www/html/" ]; then
+    	sudo cp /installer/bootstrap.ign /var/www/html/bootstrap.ign
+    	sudo sed -i -e "s|@infra_ip@|${INFRA_IP}|" /installer/sec_bootstrap.ign
+	else
+		echo "Web server folder /var/www/html not found. You must copy the boostrap.ign to web server from terraform output."
+	fi
+}
+
+function verifyInputs() {
+    if [ -z "$(echo "${DOMAIN}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}Domain name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${CLUSTER_NAME}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}Cluster name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${VCENTER}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}vCenter name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${VCENTER_USER}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}vCenter user name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${VCENTER_PASS}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}vCenter user password is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${VCENTER_DC}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}vCenter data center name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${VM_DSTORE}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}VMware data store name is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${PULL_SECRET}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}Image pull secret is missing; Exiting...${REGULAR}"
+        exit 1
+    fi
+	if [ -z "$(echo "${OCP_VERSION}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}OCP version is missing; using default version 4.2.0...${REGULAR}"
+    fi
+	if [ -z "$(echo "${CONTROL_NODES}" | tr -d '[:space:]')" ]; then
+        echo -e "${WARN}Number of control nodes is missing; using default nodes 3...${REGULAR}"
+    fi        
+}    
+
+## Gather information provided via the command line parameters
+while test ${#} -gt 0; do
+    [[ $1 =~ ^-oc|--ocversion ]]           && { OCP_VERSION="${2}";                      shift 2; continue; };
+    [[ $1 =~ ^-d|--domain ]]          && { DOMAIN="${2}";                    shift 2; continue; };
+    [[ $1 =~ ^-n|--controlnodes ]]     && { CONTROL_NODES="${2}";                     shift 2; continue; };    	
+    [[ $1 =~ ^-cn|--clustername ]]      && { CLUSTER_NAME="${2}";                shift 2; continue; };
+    [[ $1 =~ ^-vc|--vcenter ]]          && { VCENTER="${2}";              shift 2; continue; };
+    [[ $1 =~ ^-vu|--vcenteruser ]]      && { VCENTER_USER="${2}";          shift 2; continue; };
+    [[ $1 =~ ^-vp|--vcenterpassword ]]  && { VCENTER_PASS="${2}";            shift 2; continue; };
+    [[ $1 =~ ^-vd|--vcenterdatacenter ]]      && { VCENTER_DC="${2}";                shift 2; continue; };
+    [[ $1 =~ ^-vs|--vmwaredatastore ]]     && { VM_DSTORE="${2}";               shift 2; continue; };
+    [[ $1 =~ ^-s|--pullsecret ]]     && { PULL_SECRET="${2}";         shift 2; continue; };
+    [[ $1 =~ ^-h|--host ]]     && { INFRA_IP="${2}";         shift 2; continue; };	
+    break;
+done
+
+verifyInputs
+
+OCP_VERSION=${OCP_VERSION:-"4.2.0"}
+CONTROL_NODES=${CONTROL_NODES:-"3"}
+PULL_SECRET_DECODE=`echo $PULL_SECRET | base64 -d`
+gen_key
+get_installer $OCP_VERSION
+create_install_config
